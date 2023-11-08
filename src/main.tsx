f03b913010b4647b80f563f81f5c60b7b52a7393
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-09 00:31:23 +0100

    Add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import '@/index.css';
 import App from '@/App.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
