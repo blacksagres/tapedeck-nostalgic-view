79205fb697eb92e6e03bf562696886d9ff069ed6
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-08 23:47:11 +0100

    Mend type

--- src/features/tapedeck/hooks/tape.view-model.type.ts
+++ src/features/tapedeck/hooks/tape.view-model.type.ts
@@ -4,7 +4,8 @@ import type { Tape } from '../api/types/tape.type';
  * Type with playingTime property normalized to number, so the component doesn't have to deal with the string "minutes"
  * coming from the api response.
  */
-export type TapeViewModel = Tape & {
+export type TapeViewModel = Omit<Tape, 'playingTime'> & {
+  id: string;
   /**
    * The total playing time of the tape in minutes.
    */
