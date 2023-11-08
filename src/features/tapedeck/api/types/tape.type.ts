c72eeca520ea4fdcc7bdd4224f53c3e2605c5aa0
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-08 22:33:06 +0100

    Add type for api result

---
+++ src/features/tapedeck/api/types/tape.type.ts
@@ -0,0 +1,26 @@
+type Tape = {
+  page?: string;
+  img?: string;
+  thumb?: string;
+  playingTime?: string;
+  color?: Color;
+  brand?: string;
+  type?: Type;
+};
+
+type Color =
+  | 'Green'
+  | 'Black'
+  | 'Grey'
+  | 'Blue'
+  | 'White'
+  | 'Red'
+  | 'Purple'
+  | 'Yellow'
+  | 'Orange';
+
+export type Type = 'Ferro' | 'Chrome' | 'Metal' | 'Ferrochrom';
+
+export type TapeDeckApiEntry = {
+  [key: string]: Tape;
+};
