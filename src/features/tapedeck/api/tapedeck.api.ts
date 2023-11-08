62c47c3b6308fc857f708a50c1d7b2534ac9c074
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-09 00:07:19 +0100

    Use correct api key variable

--- src/features/tapedeck/api/tapedeck.api.ts
+++ src/features/tapedeck/api/tapedeck.api.ts
@@ -1,7 +1,10 @@
 import type { TapeDeckApiEntry } from '@/features/tapedeck/api/types/tape.type';
 
 export const fetchTapes = async (): Promise<TapeDeckApiEntry[]> => {
-  return await fetch('https://tapedeck-api-fresk.vercel.app/api').then(
-    (res) => res.json() as Promise<TapeDeckApiEntry[]>
-  );
+  return await fetch('https://tapedeck-api-fresk.vercel.app/api', {
+    headers: {
+      Accept: 'application/json',
+      'x-api-key': import.meta.env.VITE_API_KEY as string,
+    },
+  }).then((res) => res.json() as Promise<TapeDeckApiEntry[]>);
 };
