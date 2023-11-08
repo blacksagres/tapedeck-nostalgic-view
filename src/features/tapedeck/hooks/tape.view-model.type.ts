856c6f89677c02aec57962ec898f2e64495eb10b
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-08 23:52:34 +0100

    Add docs

--- src/features/tapedeck/hooks/tape.view-model.type.ts
+++ src/features/tapedeck/hooks/tape.view-model.type.ts
@@ -5,6 +5,9 @@ import type { Tape } from '../api/types/tape.type';
  * coming from the api response.
  */
 export type TapeViewModel = Omit<Tape, 'playingTime'> & {
+  /**
+   * A generated id based on the tape deck id and the index of the tape in the array.
+   */
   id: string;
   /**
    * The total playing time of the tape in minutes.
