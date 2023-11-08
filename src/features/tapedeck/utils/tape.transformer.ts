f684450cbbd8f955ff790e378c53c2322187cbd9
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-08 23:11:48 +0100

    Add transformer

---
+++ src/features/tapedeck/utils/tape.transformer.ts
@@ -0,0 +1,8 @@
+import type { Tape } from '@/features/tapedeck/api/types/tape.type';
+import type { TapeViewModel } from '@/features/tapedeck/hooks/tape.view-model.type';
+
+export const transformTapeApiResponseToViewModel = (
+  tapeApiEntry: TapeViewModel[]
+): Tape[] => {
+  return [];
+};
