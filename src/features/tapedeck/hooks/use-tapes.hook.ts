ffbb359856cc790d7093a78e7721b39bac8b1bde
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-08 23:59:58 +0100

    Finish hook

--- src/features/tapedeck/hooks/use-tapes.hook.ts
+++ src/features/tapedeck/hooks/use-tapes.hook.ts
@@ -1,3 +1,18 @@
-export const useTapes = () => {
-  return [];
+import type { UseQueryResult } from 'react-query';
+import { useQuery } from 'react-query';
+import { fetchTapes } from '@/features/tapedeck/api/tapedeck.api';
+import type { TapeViewModel } from '@/features/tapedeck/hooks/tape.view-model.type';
+import { transformTapeApiResponseToViewModel } from '@/features/tapedeck/utils/tape.transformer';
+
+/**
+ * Fetches the tapes from the api and transforms the response to a flat array of tapes with normalized playingTime and unique ids.
+ * @returns {UseQueryResult<TapeViewModel[], unknown>} a react-query useQuery result
+ */
+export const useTapes = (): UseQueryResult<TapeViewModel[], unknown> => {
+  const query = useQuery<TapeViewModel[]>('tapes', async () => {
+    const tapes = await fetchTapes();
+    return transformTapeApiResponseToViewModel(tapes);
+  });
+
+  return query;
 };
