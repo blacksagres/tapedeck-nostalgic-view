2a1301a947e7205803e67eb80df80977ad6772df
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-10 00:47:34 +0100

    Add pagination component

---
+++ src/components/pagination.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@/components/ui/button';
+
+type PaginationProps = {
+  setNextPage: () => void;
+  setPreviousPage: () => void;
+  nextEnabled: boolean;
+  previousEnabled: boolean;
+  startIndex: number;
+  endIndex: number;
+  totalItems: number;
+};
+
+export const Pagination = ({
+  setNextPage,
+  setPreviousPage,
+  nextEnabled,
+  previousEnabled,
+  startIndex,
+  endIndex,
+  totalItems,
+}: PaginationProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="text-sm text-muted-foreground">
+        Showing {startIndex + 1} - {endIndex} of {totalItems} tapes
+      </div>
+      <div className="flex items-center justify-end py-4 space-x-2">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={setPreviousPage}
+          disabled={!previousEnabled}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={setNextPage}
+          disabled={!nextEnabled}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
