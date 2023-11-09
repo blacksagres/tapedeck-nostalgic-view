5682c9a838859dc28f639905221068aeabf7b38e
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-09 15:01:27 +0100

    Use sorter function

--- src/features/tapedeck/hooks/use-tapes.hook.ts
+++ src/features/tapedeck/hooks/use-tapes.hook.ts
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query';
 import { fetchTapes } from '@/features/tapedeck/api/tapedeck.api';
 import type { TapeViewModel } from '@/features/tapedeck/hooks/tape.view-model.type';
 import { transformTapeApiResponseToViewModel } from '@/features/tapedeck/utils/tape.transformer';
+import { sortByBrand } from '@/features/tapedeck/hooks/utils/tape.view-model.utils';
 
 /**
  * Fetches the tapes from the api and transforms the response to a flat array of tapes with normalized playingTime and unique ids.
@@ -11,7 +12,8 @@ import { transformTapeApiResponseToViewModel } from '@/features/tapedeck/utils/t
 export const useTapes = (): UseQueryResult<TapeViewModel[], unknown> => {
   const query = useQuery<TapeViewModel[]>('tapes', async () => {
     const tapes = await fetchTapes();
-    return transformTapeApiResponseToViewModel(tapes);
+    const tapesViewModels = transformTapeApiResponseToViewModel(tapes);
+    return tapesViewModels.sort(sortByBrand);
   });
 
   return query;
