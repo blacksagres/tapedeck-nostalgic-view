bc5853e545310b1b37f6e6124d5e075aef4a3e09
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-09 14:59:31 +0100

    Add sorter utils

---
+++ src/features/tapedeck/hooks/utils/tape.view-model.utils.ts
@@ -0,0 +1,8 @@
+import type { TapeViewModel } from '@/features/tapedeck/hooks/tape.view-model.type';
+
+export const sortByBrand = (a: TapeViewModel, b: TapeViewModel) => {
+  if (!a.brand) return 0;
+  if (!b.brand) return 0;
+
+  return a.brand.localeCompare(b.brand);
+};
