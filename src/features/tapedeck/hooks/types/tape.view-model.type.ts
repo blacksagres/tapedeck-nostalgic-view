b901f7c09e98e9b2ac05a226e9ee4466e3ab7f0a
Geraldo <geraldo.nogueirabs@gmail.com>
2023-11-09 18:22:17 +0100

    Remove unnecessary type intersection

--- src/features/tapedeck/hooks/types/tape.view-model.type.ts
+++ src/features/tapedeck/hooks/types/tape.view-model.type.ts
@@ -1,10 +1,8 @@
-import type { TapeApiEntry } from '@/features/tapedeck/api/types/tape.type';
-
 /**
  * Type with playingTime property normalized to number, so the component doesn't have to deal with the string "minutes"
  * coming from the api response.
  */
-export type TapeViewModel = Omit<TapeApiEntry, 'playingTime'> & {
+export type TapeViewModel = {
   /**
    * A generated id based on the tape deck id and the index of the tape in the array.
    */
